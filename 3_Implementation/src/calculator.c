d628da899f739dbeb338e01be43ad006fcd5027c
SUMEETGOKHARE <98872208+sumeet2908@users.noreply.github.com>
2022-02-24 12:19:47 +0530

    Update calculator.c

--- 3_Implementation/src/calculator.c
+++ 3_Implementation/src/calculator.c
@@ -1,84 +1,25 @@
- /**
- * @file project_main.c
- * @author SUMEET ()
- * @brief 
- * @version 0.1
- * @date 2022-02-10
- * 
- * @copyright Copyright (c) 2022
- * 
- */
+#include "calculator.h"
 
-int main()
-{
-    // variables
-    int ch;
-    do
-    {
-        // Calculator display
-        printf (" Select an operation to perform the calculation in C Calculator: ");
-        printf (" \n 1-Addition  \t \t 2-Subtraction \n 3-Multiplication \t 4-Division \n 5-Square \t \t 6-modulus \n 7-Exit \n \n ## Make your choice=");
+int addition();
+int subtract();
+int multiply();
+int divide();
+int sq();
+int mod();
+void exit();
 
-        scanf ("%d", &ch); // choice the operation
-
-
-    // switch
-    switch (ch)
-    {
-        case 1:
-            addition(); // call the addition()
-            break; // break the function
-
-        case 2:
-            subtract(); // call the subtract()
-            break; // break the function
-
-        case 3:
-            multiply(); // call the multiply()
-            break; // break the function
-
-        case 4:
-            divide(); // call the divide()
-            break; // break the function
-
-        case 5:
-            sq(); // call the sq()
-            break; // break the function
-
-        case 6:
-            mod(); // It call the sqrt1()
-            break; // break the function
-
-        case 7:
-            exit(0); //call the exit()
-            break; // break the function
-
-        default:
-            printf(" Something is wrong!! ");
-            break;
-    }
-    printf (" \n \n **************** \n ");
-    } while (ch != 7);
-
-
-    return 0;
-}
-
-
-
-// function definition
 int addition()
 {
     int i, sum = 0, num, f_num; // declare a local variable
     printf (" How many numbers you want to add: ");
     scanf ("%d", &num);
-    printf (" Enter the numbers: \n ");
+    printf (" Enter the numbers:\n ");
     for (i = 1; i <= num; i++)
     {
         scanf(" %d", &f_num);
         sum = sum + f_num;
     }
-    printf (" Total Sum of the numbers = %d", sum);
+    printf (" Total Sum of the numbers = %d\n", sum);
     return 0;
 }
 
@@ -135,16 +76,16 @@ int sq()
     printf (" \n The Square of %d is: %d", n1, result);
 }
 
-// use sqrt1() to get the square root of the given number
+// use mod() to get the mod of the given number
 int mod()
 {
     float result;
     int n1,n2;
-    printf (" Enter a number to get the Square Root: ");
+    printf (" Enter a number to get the mod: ");
     scanf ("  %d", &n1);
     printf (" The second number is: ");
     scanf ("  %d", &n2);
 
     result = n1%n2;
-    printf (" \n The Square Root of %d is: %f", n1, result);
+    printf (" \n The mod of %d is: %f", n1, result);
 }
