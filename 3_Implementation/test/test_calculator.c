ef234b07c8319a308794207326fbc1b2f3c8a774
SUMEETGOKHARE <98872208+sumeet2908@users.noreply.github.com>
2022-02-24 12:21:10 +0530

    Update test_calculator.c

--- 3_Implementation/test/test_calculator.c
+++ 3_Implementation/test/test_calculator.c
@@ -1,6 +1,7 @@
 #include "unity.h"
+#include "calculator.h"
 
-void setup()
+void setUp()
 {
 
 }
@@ -10,21 +11,20 @@ void tearDown()
 
 }
 
-void test_divide()
+void test_addition(void)
 {
-    int divide_n1=4;
-    int divide_n2=2;
-    int actual_result=divide_n1/divide_n2;
+    int n1;
+    int n2;
+    int actual_result=n1+n2;
      
-    TEST_ASSERT_EQUAL(success, test_divide(divide_n1,divide_n2, &actual_result)); 
+    TEST_ASSERT_EQUAL(addition(n1,n2),actual_result); 
 }
-
 int test_main()
 {
     /* Initiate the Unity Test Framework */
     UNITY_BEGIN();
 
-    RUN_TEST(test_divide);
+    RUN_TEST(test_addition);
 
       return UNITY_END();
 }
